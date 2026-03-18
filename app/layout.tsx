19a69a71b9abde896f82d86641f1b6f8487fc0fc
518luck <1512013298@qq.com>
2026-03-18 23:41:07 +0800

    feat: 项目初始化

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import './style.css'
+import Sidebar from '@/components/Sidebar'
+
+export default async function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+
+  return (
+    <html lang="en">
+      <body>
+        <div className="container">
+          <div className="main">
+            <Sidebar />
+            <section className="col note-viewer">{children}</section>
+
+          </div>
+
+        </div>
+
+      </body>
+
+    </html>
+
+  )
+}
